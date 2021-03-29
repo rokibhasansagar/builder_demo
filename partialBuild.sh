a691804281b66740a703150559b01a6a3d152cf8
Rokib Hasan Sagar <rokibhasansagar2014@outlook.com>
2021-03-29 12:04:34 +0600

    Update partialBuild.sh

--- partialBuild.sh
+++ partialBuild.sh
@@ -4,7 +4,7 @@ cd ~/rom || exit 1
 
 export ALLOW_MISSING_DEPENDENCIES=true
 
-echo "::group::Lunch for ${{ env.partialTarget }}"
+echo "::group::Lunch for ${partialTarget}"
 . build/envsetup.sh
 lunch aosp_mido-user
 echo "::endgroup::"
@@ -16,11 +16,12 @@ df -hlT
 echo "::endgroup::"
 
 _ccache_stats() {
-  sleep 1m
-  while true; echo -e "CCACHE_STAT\n"; do ccache -s; echo -e "\n"; top -b -i -n 1; sleep 5m; done
+  sleep 5m
+  while true; do echo -e "CCACHE_STAT\n"; ccache -s; echo -e "\n"; top -b -i -n 1; sleep 5m; done
 }
 
 _ccache_stats 2>/dev/null &
+
 {
   make -j4 ${1} || mmma ${1}
 } || exit 1
