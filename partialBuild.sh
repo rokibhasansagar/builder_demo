be422f0235a699034073301914b3ff58c798cd84
Rokib Hasan Sagar <rokibhasansagar2014@outlook.com>
2021-03-29 17:27:27 +0600

    Update partialBuild.sh

--- partialBuild.sh
+++ partialBuild.sh
@@ -15,6 +15,11 @@ echo "::group::Disk Space"
 df -hlT
 echo "::endgroup::"
 
+echo "::group::Ccache Size before build"
+du -sh ~/.cache/ccache 2>/dev/null || true
+echo ":endgroup::"
+
+echo "::group::Build Process"
 _ccache_stats() {
   sleep 5m
   while true; do echo -e "CCACHE_STAT\n"; ccache -s; echo -e "\n"; top -b -i -n 1; sleep 5m; done
@@ -26,4 +31,10 @@ _ccache_stats 2>/dev/null &
   make -j4 ${1} || mmma ${1}
 } || exit 1
 kill %1
+echo "::endgroup::"
+
+echo "::group::Ccache Size before build"
+du -sh ~/.cache/ccache 2>/dev/null || true
+echo ":endgroup::"
+
 exit
