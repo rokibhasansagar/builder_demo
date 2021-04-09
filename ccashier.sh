d41ee714383f6c7c3aa415409e42ce571f623dbc
Rokib Hasan Sagar <rokibhasansagar2014@outlook.com>
2021-04-09 15:33:18 +0600

    Update ccashier.sh
    - more debug on ccache

--- ccashier.sh
+++ ccashier.sh
@@ -1,20 +1,30 @@
 #!/bin/bash
+set -v
 
 ccache_task="${1}"    # upload/download
 
 CCache_URL="https://gdrive.phantomzone.workers.dev/0:/mido_ccache/ccache.tgz"
 
 if [[ ${ccache_task} =~ upload ]]; then
-  tar -I "pigz -k -2" -cf ~/.cache/ccache.tgz ~/.cache/ccache
-  mkdir -p ~/.config/rclone
-  echo "${RClone_Config}" > ~/.config/rclone/rclone.conf
+  printf "Compressing ccache data...\n"
+  ccache -s
+  tar -I "pigz -k -3" -cf /home/runner/.cache/ccache.tgz /home/runner/.cache/ccache
+  du -sh /home/runner/.cache/ccache.tgz
+  printf "Setting up rclone...\n"
+  mkdir -p /home/runner.config/rclone
+  echo "${RClone_Config}" > /home/runner.config/rclone/rclone.conf
   rclone delete td:/mido_ccache/ccache.tgz 2>/dev/null || true
-  rclone copy ~/.cache/ccache.tgz td:/mido_ccache/ --progress
-  rm -rf ~/.cache/ccache.tgz
+  rclone copy /home/runner/.cache/ccache.tgz td:/mido_ccache/ --progress
+  rm -rf /home/runner/.cache/ccache.tgz
 elif [[ ${ccache_task} =~ download ]]; then
-  mkdir -p ~/.cache/ccache
-  cd ~/.ccache || exit 1
+  printf "Downloading previous ccache...\n"
+  mkdir -p /home/runner/.cache/ccache
+  cd /home/runner/.ccache || exit 1
   aria2c -c -x16 -s16 "${CCache_URL}"
   tar -I "pigz" -xf ccache.tgz
-  rm -rf ~/.cache/ccache.tgz
+  ls -lAog /home/runner/.cache/ccache
+  ccache -s
+  rm -rf /home/runner/.cache/ccache.tgz
 fi
+
+set +v
